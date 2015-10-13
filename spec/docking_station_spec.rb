0aa9a30667a914f2914825d0c5eaa8017b546b6a
Robert Youngman <rob_youngman@yahoo.com>
2015-10-13 17:06:26 +0100

    Chpt 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,9 @@ describe DockingStation do
   it 'expects DockingStation to respond to method release_bike' do
       expect(subject).to respond_to :release_bike
   end
+
+  it 'expects released bike to be a working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
